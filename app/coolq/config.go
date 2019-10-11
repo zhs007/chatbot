18266dba9271121a734ac5954f7246f051868af9
zhs007 <sssxueren@gmail.com>
2019-10-11 20:31:45 +0800

    - added coolqbot

---
+++ app/coolq/config.go
@@ -0,0 +1,70 @@
+package chatbotcoolq
+
+import (
+	"io/ioutil"
+	"os"
+
+	chatbotbase "github.com/zhs007/chatbot/base"
+	"gopkg.in/yaml.v2"
+)
+
+// Config - configuate
+type Config struct {
+	CoolQToken        string
+	CoolQSecret       string
+	CoolQServURL      string
+	CoolQHttpServAddr string
+	Token             string
+	ServAddr          string
+	Username          string
+	Debug             bool
+	PreloadUserInfo   bool
+}
+
+func checkConfig(cfg *Config) error {
+	if cfg.CoolQServURL == "" {
+		return chatbotbase.ErrNoCoolQServURL
+	}
+
+	if cfg.CoolQHttpServAddr == "" {
+		return chatbotbase.ErrNoCoolQHttpServAddr
+	}
+
+	if cfg.Username == "" {
+		return chatbotbase.ErrNoUsername
+	}
+
+	if cfg.ServAddr == "" {
+		return chatbotbase.ErrNoServAddr
+	}
+
+	return nil
+}
+
+// LoadConfig - load config
+func LoadConfig(filename string) (*Config, error) {
+	fi, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &Config{}
+
+	err = yaml.Unmarshal(fd, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	err = checkConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20190926025831-c00fd9afed17
 	golang.org/x/text v0.3.2
+	google.golang.org/appengine v1.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20190227213309-4f5b463f9597 // indirect
 	google.golang.org/grpc v1.22.1
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
