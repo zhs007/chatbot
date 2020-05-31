1bbfaff871dbbe82ee59b810d23e63cb3f9c080e
zhs007 <sssxueren@gmail.com>
2020-05-31 22:25:45 +0800

    - Fixed some bugs in procRegexpNode

--- plugins/preproc/utils_test.go
+++ plugins/preproc/utils_test.go
@@ -31,7 +31,7 @@ func Test_procRegexpNode(t *testing.T) {
 		t.Fatalf("Test_procRegexpNode procRegexpNode %v", err)
 	}
 
-	if ret.Msg != "note -m search -n comic  -k 北斗神拳" {
+	if ret.Msg != "note -m search -n comic  -k \"北斗神拳\"" {
 		t.Fatalf("Test_procRegexpNode procRegexpNode fail. %v", ret.Msg)
 	}
 
@@ -44,7 +44,20 @@ func Test_procRegexpNode(t *testing.T) {
 		t.Fatalf("Test_procRegexpNode procRegexpNode %v", err)
 	}
 
-	if ret.Msg != "note -m search -n comic  -k 北斗神拳 -k 原哲夫" {
+	if ret.Msg != "note -m search -n comic  -k \"北斗神拳\" -k \"原哲夫\"" {
+		t.Fatalf("Test_procRegexpNode procRegexpNode fail. %v", ret.Msg)
+	}
+
+	chat = &chatbotpb.ChatMsg{
+		Msg: "很难找",
+	}
+
+	ret, err = procRegexpNode(rn, chat)
+	if err != nil {
+		t.Fatalf("Test_procRegexpNode procRegexpNode %v", err)
+	}
+
+	if ret != nil {
 		t.Fatalf("Test_procRegexpNode procRegexpNode fail. %v", ret.Msg)
 	}
 
@@ -80,4 +93,87 @@ func Test_procRegexpNode2(t *testing.T) {
 	}
 
 	t.Log("Test_procRegexpNode2 OK")
+}
+
+func Test_procRegexpNode3(t *testing.T) {
+	rn := &RegexpNode{
+		Pattern:          "找+(.*)",
+		Prefix:           "note -m search -n comic ",
+		Mode:             "paramarray",
+		ParamArrayPrefix: " -k ",
+	}
+
+	r, err := regexp.Compile(rn.Pattern)
+	if err != nil {
+		t.Fatalf("Test_procRegexpNode3 Compile %v", err)
+	}
+
+	rn.r = r
+
+	chat := &chatbotpb.ChatMsg{
+		Msg: "很难找",
+	}
+
+	ret, err := procRegexpNode(rn, chat)
+	if err != nil {
+		t.Fatalf("Test_procRegexpNode3 procRegexpNode %v", err)
+	}
+
+	if ret != nil {
+		t.Fatalf("Test_procRegexpNode3 procRegexpNode fail. %v", ret.Msg)
+	}
+
+	chat = &chatbotpb.ChatMsg{
+		Msg: "找漫画北斗神拳 原哲夫",
+	}
+
+	ret, err = procRegexpNode(rn, chat)
+	if err != nil {
+		t.Fatalf("Test_procRegexpNode procRegexpNode %v", err)
+	}
+
+	if ret.Msg != "note -m search -n comic  -k \"漫画北斗神拳\" -k \"原哲夫\"" {
+		t.Fatalf("Test_procRegexpNode procRegexpNode fail. %v", ret.Msg)
+	}
+
+	chat = &chatbotpb.ChatMsg{
+		Msg: "找找漫画北斗神拳 原哲夫",
+	}
+
+	ret, err = procRegexpNode(rn, chat)
+	if err != nil {
+		t.Fatalf("Test_procRegexpNode procRegexpNode %v", err)
+	}
+
+	if ret.Msg != "note -m search -n comic  -k \"漫画北斗神拳\" -k \"原哲夫\"" {
+		t.Fatalf("Test_procRegexpNode procRegexpNode fail. %v", ret.Msg)
+	}
+
+	chat = &chatbotpb.ChatMsg{
+		Msg: "给我好好找找  ",
+	}
+
+	ret, err = procRegexpNode(rn, chat)
+	if err != nil {
+		t.Fatalf("Test_procRegexpNode procRegexpNode %v", err)
+	}
+
+	if ret != nil {
+		t.Fatalf("Test_procRegexpNode procRegexpNode fail. %v", ret.Msg)
+	}
+
+	chat = &chatbotpb.ChatMsg{
+		Msg: "找找\"漫画北斗神拳 原\"哲夫",
+	}
+
+	ret, err = procRegexpNode(rn, chat)
+	if err != nil {
+		t.Fatalf("Test_procRegexpNode procRegexpNode %v", err)
+	}
+
+	if ret.Msg != "note -m search -n comic  -k \"漫画北斗神拳 原\" -k \"哲夫\"" {
+		t.Fatalf("Test_procRegexpNode procRegexpNode fail. %v", ret.Msg)
+	}
+
+	t.Log("Test_procRegexpNode3 OK")
 }
